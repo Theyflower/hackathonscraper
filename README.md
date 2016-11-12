5af66239c92fdf941c557cb0d64727eab9f940f3
Aaron Thomas <1d20m6@gmail.com>
2016-11-12 18:02:35 -0500

    updated readme and the incomplete code is less incomplete

--- README.md
+++ README.md
@@ -3,5 +3,12 @@ A script that scrapes devpost for a list of hackathons and puts them in a nice J
 
 # Dependencies
 * python3
-<!-- * beautifulsoup (DO WE NEED IT?) -->
+* bs4
 * mechanicalsoup
+
+Resolving the dependencies:
+python3 : I'm not gonna tell you how to do this, you should know
+
+You may need to run these commands as root (you almost certainly need to.)
+bs4 : $ pip install bs4
+mechanical soup : $ pip install mechanicalsoup
--- src/hackscraper.py
+++ src/hackscraper.py
@@ -1,16 +1,21 @@
+#!/usr/bin/python
 import mechanicalsoup
+from BeautifulSoup import BeautifulSoup as soup
 
 browser = mechanicalsoup.Browser()
 
-def populate_pagelist(pagedata):
+def populate_pagelist(page):
 	'''
 	how do i docstrings good?
 	takes a page and then gives you an int list of the pages containing data we need to scrape aw yeah
 	'''
-	page = browser.get
-	the_div = page.soup.find('div', class_='pagination')
+
+	the_div = soup(page.soup.find('div', class_='pagination'))
+#	links = the_div.soup.find_all('a')
+	print(the_div)
 	#pagination is the classname of stuff
-	pass
+
+	return range(10)#this is temporary
 
 def populate_hackathonlist(pdagedata):
 	'''
@@ -19,13 +24,14 @@ def populate_hackathonlist(pdagedata):
 	'''
 	pass
 
+
 base_url = "https://devpost.com/hackathons?utf8=✓&search=&challenge_type=in-person&sort_by=Submission+Deadline&page="
 maxpage = 9001 #this script will break if there are over nine thousand pages of hackathons
 
 #make sure maxpage is higher than the amount of pages there actually are
  
 current_url = "{}{}".format(base_url,maxpage) #classname pagination has the page 
-current_page = broswer.get(current_url)
+current_page = browser.get(current_url)
 
 #populate a list of pages
 
