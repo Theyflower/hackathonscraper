c4781a8374eb71f68125e73d0367d3b1c167ac65
Aaron Thomas <1d20m6@gmail.com>
2016-11-12 21:51:21 -0500

    more partial implementation of scrape_hackathons() just so that if this is never finished someone can pickup my incomplete code and finish it

--- src/hackscraper.py
+++ src/hackscraper.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
+"""
+congratulations, you are reading my code
+there are a LOT of REALLY BADly named variables
+if you think you know a better name for a variable then you are probably right
+please fork this repo and make a pull request with refactored code
+
+also make good docstrings because idk how to do that
+"""
 import mechanicalsoup
 from bs4 import BeautifulSoup as soup #might actually not need this
 
@@ -14,18 +22,37 @@ def populate_pagelist(page):
 	links = [int(link.string) for link in links if link.string is not None] #extracts the number from them, one of them is None so we do the is not None
 	return links #yatta
 
-def scrape_hackathon(page):
+def scrape_hackathons(page):
 	'''
 	takes a page and then scrapes all the hackathons from it and then puts them into a list or dictionary or something
 	its gonna be awesome
+	the variable names in this
 	'''
-	#in theory this module works if this function is implemented.
-	#im gonna celebrate my accomplishments for a bit (until after I've eaten dinner)
-	#this is the hard part though :(
-	#but i'm coding something useful that someone else will probably use :)
+	hackathons = []
+	desired_classnames = ["challenge-location","title","value date-range","challenge-description","thumbnail_image image-replacement"] 
+	hackathon_template = {'name' : None, 'location' : None, 'desc': None, 'date' : {'start' : None, 'end' : None}, 'logo' : None	}
+	#
+	# hacksdata = page.soup.findAll('article',class_='challenge-listing')
+	tags = page.soup.findAll(True, {'class':desired_classnames})
 
-	#so classnames with "challenge-listing " have the data that i need
-	pass
+	ZERO_CLASS = tags[0]['class']
+	hackathon_data = []
+	for tag in tags:
+		if tag['class'] == ZERO_CLASS:
+			if 'curr_hackathon' in locals():
+				hackathon_data.append(current_hackathon)
+			current_hackathon = []
+		current_hackathon.append(tag)
+
+	#yeah so the variable current_hackathon is going to be repurposed
+	#if you have good ideas for better variable names please refactor my code
+	#i will accept pull requests that improve variable names
+
+	for hackathon_tags in hackathon_data:
+		current_hackathon = deepcopy(hackathon_template)
+		print(hackathon_tags)
+
+	return range(10) #this is temporary
 
 
 base_url = "https://devpost.com/hackathons?utf8=✓&search=&challenge_type=in-person&sort_by=Submission+Deadline&page="
@@ -38,12 +65,14 @@ current_page = browser.get(current_url)
 
 #populate a list of pages
 
-pages = populate_pagelist(current_page) #gvn is an acronum for good variable name #todo(aaron) make up a better one
+pageids = populate_pagelist(current_page) #gvn is an acronum for good variable name #todo(aaron) make up a better one
 
 hackathons = []
-for page in pages:
-	current_url = "{}{}".format(base_url,page)
-	hackathons.append(scrape_hackathon(page))
+for pageid in pageids:
+	current_url = "{}{}".format(base_url,pageid)
+	page = browser.get(current_url)
+	for hackathon in scrape_hackathons(page):
+		hackathons.append(hackathon)
 	#get the page
 	#scrape it good
 
